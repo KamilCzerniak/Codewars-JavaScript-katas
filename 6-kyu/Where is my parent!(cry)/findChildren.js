da2c0fe4667c57f36f7f39eafa578ee5fa1caa97
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-12-07 15:08:37 +0100

    Completed

---
+++ 6-kyu/Where is my parent!(cry)/findChildren.js
@@ -0,0 +1,5 @@
+function findChildren(dancingBrigade) {
+    return dancingBrigade.split("")
+        .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()) || b.localeCompare(a))
+        .join("");
+}
