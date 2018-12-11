aaeceed88783cf8f43b4a004580f959da40ebc37
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-12-11 01:40:37 +0100

    Applied small changes

--- 6-kyu/Single Word Pig Latin/PigLatin.js
+++ 6-kyu/Single Word Pig Latin/PigLatin.js
@@ -2,10 +2,12 @@ function pigLatin(string) {
     string = string.toLowerCase();
     if ((/[^a-z]/g).test(string)) {
         return null;
-    } else if (s[0].match(/[aeiou]/g)) {
-        return s + "way";
+    } else if (string[0].match(/[aeiou]/g)) {
+        return string + "way";
     } else {
-        let index = s.search(/[aeiou]/);
-        return s.substring(index) + s.substring(0, index) + "ay";
+        let index = string.search(/[aeiou]/);
+        return string.substring(index) + string.substring(0, index) + "ay";
     }
 }
+
+console.log(pigLatin("spaghetti"));
