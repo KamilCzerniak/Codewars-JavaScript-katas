9870b462c16fef586b5bbcc4e3b60c530672a355
KamilCzerniak <kamil.z.czerniak@gmail.com>
2019-01-05 18:20:37 +0100

    Completed

---
+++ 8-kyu/For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre/quote.js
@@ -0,0 +1,5 @@
+var quote = function (fighter) {
+    return fighter.match(/conor mcgregor/i)
+        ? "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+        : "I am not impressed by your performance.";
+};
